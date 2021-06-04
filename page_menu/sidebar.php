345d81e743ff79b38249040d930b6c84eaed3c5f
primarazaq <primarazaq34@gmail.com>
2021-06-04 19:18:47 +0700

    ..

--- page_menu/sidebar.php
+++ page_menu/sidebar.php
@@ -1,18 +1,34 @@
-<div class="col-md-2 tosca text-white">
+<div class="col-md-2 tosca ">
     <nav id="sidebar" class="active">
         <br>
-        <ul class="list-unstyled components mb-5">
-            <font size=4 color=white>
-            <li class="active">
-                <a href="#"><span class="fa fa-home"></span> Daftar Transaksi</a>
-            </li>
-            <li>
-                <a href="#"><span class="fa fa-user"></span> Daftar Menu</a>
-            </li>
-            <li>
-                <a href="#"><span class="fa fa-sticky-note"></span> Profil</a>
-            </li>
-            </font>
+        <ul class="navbar-nav ml-md-auto margin">
+      
+                <li class="nav-item">
+                    <div>
+                    
+                        <button class="btn text-white btn-outline-light">
+                        <h6><span class="bi bi-list-ul"></span>
+                        <span class="">Daftar Transaksi</span></h6>
+                        
+                        </button>
+                    </div>
+                </li>
+                <li class="nav-item">
+                     <div>
+                        <button class="btn text-white btn-outline-light"><h6>Daftar Menu</h6>
+                            <span class=""></span>
+                           
+                        </button>
+                    </div>
+                </li>
+                <li class="nav-item">
+                     <div>
+                        <button class="btn text-white btn-outline-light"><h6>Profil</h6>
+                            <span class=""></span>
+                           
+                        </button>
+                    </div>
+                </li>   
         </ul>
     </nav>
 </div>
