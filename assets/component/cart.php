981adc703736ade067438d2b5ce1c2025b3e0d69
angjaay <angga.angzai@gmai.com>
2021-06-04 19:24:41 +0700

    add cart

---
+++ assets/component/cart.php
@@ -0,0 +1,13 @@
+<div class="card">
+    <h5 class="card-header">
+        Card title
+    </h5>
+    <div class="card-body">
+        <p class="card-text">
+            Card content
+        </p>
+    </div>
+    <div class="card-footer">
+        Card footer
+    </div>
+</div>
--- index.php
+++ index.php
@@ -48,19 +48,7 @@
                 </div>
                 <div class="col-md-4">
 
-                    <div class="card">
-                        <h5 class="card-header">
-                            Card title
-                        </h5>
-                        <div class="card-body">
-                            <p class="card-text">
-                                Card content
-                            </p>
-                        </div>
-                        <div class="card-footer">
-                            Card footer
-                        </div>
-                    </div>
+                    <?php include 'assets/component/cart.php' ?>
                 </div>
 
 
