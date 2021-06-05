690ff9ef19eaebe9fcd17b8b177133620297da8e
angjaay <angga.angzai@gmai.com>
2021-06-05 10:17:30 +0700

    edit tabel

--- page_menu/tabel2.php
+++ page_menu/tabel2.php
@@ -2,103 +2,121 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page Admin</title>
-    <link rel="stylesheet" href="assets/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Page Admin</title>
+  <link rel="stylesheet" href="assets/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
-    <style>
-        #sidebar {
-            min-height: 100vh;
-        }
-        .napbar {
-            min-width: 90hv;
-        }
-    </style>
+  <style>
+    #sidebar {
+      min-height: 100vh;
+    }
+
+    .napbar {
+      min-width: 90hv;
+    }
+  </style>
 </head>
-<body>
-<div class="container w-50">         
-  <table class="table table-bordered">
-  <br>
-    <thead>
-      <tr>
-        
-        <th><center>No</center></th>
-        <th><center>Gambar</center></th>
-        <th><center>Ruang</center></th>
-        <th><center>Aksi</center></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td><center>1</center></td>
-        <td></td>
-        <td>
-            Nama :
-            <br>
-            Deskripsi :
-            <br>
-            Harga :
-            <br>
-        </td>
-        <td>
-        <button type="button" class="btn btn-success btn-block">edit</button>
-        <button type="button" class="btn btn-danger btn-block">hapus</button>
-        </td>
-      </tr>
-      <tr>
-        <td><center>2</center></td>
-        <td></td>
-        <td>
-            Nama :
-            <br>
-            Deskripsi :
-            <br>
-            Harga :
-            <br>
-        </td>
-        <td>
-        <button type="button" class="btn btn-success btn-block">edit</button>
-        <button type="button" class="btn btn-danger btn-block">hapus</button>
-        </td>
-      </tr>
-      <tr>
-        <td><center>3</center></td>
-        <td></td>
-        <td>
-            Nama :
-            <br>
-            Deskripsi :
-            <br>
-            Harga :
-            <br>
-        </td>
-        <td>
-        <button type="button" class="btn btn-success btn-block">edit</button>
-        <button type="button" class="btn btn-danger btn-block">hapus</button>
-        </td>
-      </tr>
-      <tr>
-        <td><center>4</center></td>
-        <td></td>
-        <td>
-            Nama :
-            <br>
-            Deskripsi :
-            <br>
-            Harga :
-            <br>
-        </td>
-        <td>
-        <button type="button" class="btn btn-success btn-block">edit</button>
-        <button type="button" class="btn btn-danger btn-block">hapus</button>
-        </td>
-      </tr>
 
-</div>
+<body>
+  <div class="container w-auto">
+    <table class="table table-bordered">
+      <br>
+      <thead>
+        <tr>
+
+          <th>
+            <center>No</center>
+          </th>
+          <th>
+            <center>Gambar</center>
+          </th>
+          <th>
+            <center>Ruang</center>
+          </th>
+          <th>
+            <center>Aksi</center>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <center>1</center>
+          </td>
+          <td></td>
+          <td>
+            Nama :
+            <br>
+            Deskripsi :
+            <br>
+            Harga :
+            <br>
+          </td>
+          <td>
+            <button type="button" class="btn btn-success btn-block">edit</button>
+            <button type="button" class="btn btn-danger btn-block">hapus</button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <center>2</center>
+          </td>
+          <td></td>
+          <td>
+            Nama :
+            <br>
+            Deskripsi :
+            <br>
+            Harga :
+            <br>
+          </td>
+          <td>
+            <button type="button" class="btn btn-success btn-block">edit</button>
+            <button type="button" class="btn btn-danger btn-block">hapus</button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <center>3</center>
+          </td>
+          <td></td>
+          <td>
+            Nama :
+            <br>
+            Deskripsi :
+            <br>
+            Harga :
+            <br>
+          </td>
+          <td>
+            <button type="button" class="btn btn-success btn-block">edit</button>
+            <button type="button" class="btn btn-danger btn-block">hapus</button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <center>4</center>
+          </td>
+          <td></td>
+          <td>
+            Nama :
+            <br>
+            Deskripsi :
+            <br>
+            Harga :
+            <br>
+          </td>
+          <td>
+            <button type="button" class="btn btn-success btn-block">edit</button>
+            <button type="button" class="btn btn-danger btn-block">hapus</button>
+          </td>
+        </tr>
+
+  </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
