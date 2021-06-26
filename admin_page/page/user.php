bd1e56ac60e6cac65f8735e00c2daf64d0a5ee91
primarazaq <primarazaq34@gmail.com>
2021-06-26 10:36:13 +0700

    user

---
+++ admin_page/page/user.php
@@ -0,0 +1,41 @@
+
+<div class="row">
+    <!-- Column -->
+    <!-- Column -->
+    <div class="">
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive align-items-center">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr align="center">
+                                <th class="border-top-0" style="width: 5%;">#</th>
+                                <th class="border-top-0" style="width: 20%;">Name</th>
+                                <th class="border-top-0" style="width: 20%;">Username</th>
+                                <th class="border-top-0" style="width: 20%;">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                                <tr>
+                                    <td align="center" class="align-middle"></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td align="center" class="align-middle">
+                                        <a href="edit_user.php">
+                                            <button type="button" class="btn btn-success btn-block align-items-right text-white">Edit</button>
+                                        </a>
+                                        <a href="delete_user.php">
+                                            <button type="button" class="btn btn-danger btn-block text-white">Delete</button>
+                                        </a>
+                                    </td>
+                                </tr>
+                    
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Column -->
+</div>
+<!-- Row -->
