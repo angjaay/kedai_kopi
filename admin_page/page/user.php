14b549f76129d11f23ff56e8b7b9bb4433e5dab3
primarazaq <primarazaq34@gmail.com>
2021-06-26 10:45:17 +0700

    update page user

--- admin_page/page/user.php
+++ admin_page/page/user.php
@@ -1,3 +1,14 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 <div class="row">
     <!-- Column -->
@@ -21,12 +32,15 @@
                                     <td></td>
                                     <td></td>
                                     <td align="center" class="align-middle">
-                                        <a href="edit_user.php">
-                                            <button type="button" class="btn btn-success btn-block align-items-right text-white">Edit</button>
-                                        </a>
-                                        <a href="delete_user.php">
-                                            <button type="button" class="btn btn-danger btn-block text-white">Delete</button>
-                                        </a>
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                                        Action
+                                        </button>
+                                        <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="edit_user.php">Edit</a>
+                                        <a class="dropdown-item" href="delete_user.php">Delete</a>
+                                        </div>
+                                    </div>
                                     </td>
                                 </tr>
                     
