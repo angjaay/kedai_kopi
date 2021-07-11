1d95310764defb972e461d37dcdf2d14d6ebc5a0
primarazaq <primarazaq34@gmail.com>
2021-07-12 02:36:13 +0700

    fixed position & add sweetalert logout

--- index.php
+++ index.php
@@ -195,7 +195,7 @@ if ($_SESSION['login']) {
                         <?php } ?>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-sm-3" style="position: fixed; margin-left:900px;">
                     <?php include 'assets/component/cart.php' ?>
                 </div>
             </div>
@@ -213,6 +213,38 @@ if ($_SESSION['login']) {
                 type: 'danger',
                 confirmButtonClass: "btn-danger"
             })
+        }
+
+        function keluar(ev) {
+            ev.preventDefault();
+            var urlToRedirect = ev.currentTarget.getAttribute('href'); //use currentTarget because the click may be on the nested i tag and not a tag causing the href to be empty
+            console.log(urlToRedirect); // verify if this is the right URL
+            Swal.fire({
+                    title: "Kamu yakin ingin keluar?",
+                    icon: "warning",
+                    showDenyButton: true,
+                    confirmButtonText: `Ya`,
+                    dangerMode: true,
+                    denyButtonText: `Tidak`,
+
+                    allowOutsideClick: () => {
+                        const popup = Swal.getPopup()
+                        popup.classList.remove('swal2-show')
+                        setTimeout(() => {
+                            popup.classList.add('animate__animated', 'animate__headShake')
+                        })
+                        setTimeout(() => {
+                            popup.classList.remove('animate__animated', 'animate__headShake')
+                        }, 500)
+                        return false
+                    }
+                })
+                .then((willQuit) => {
+                    // redirect with javascript here as per your logic after showing the alert using the urlToRedirect value
+                    if (willQuit.isConfirmed) {
+                        window.location = urlToRedirect;
+                    }
+                });
         }
     </script>
 </body>
