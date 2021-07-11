8e4f7caca79196f2c2abef23c0b4c467639e45e6
primarazaq <primarazaq34@gmail.com>
2021-07-11 12:35:22 +0700

    add link to edit_user (not working)

--- admin_page/page/user.php
+++ admin_page/page/user.php
@@ -1,16 +1,6 @@
 <?php $data_kasir = $user->get_all(); ?>
 
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 <div class="row">
     <!-- Column -->
@@ -37,11 +27,15 @@
                                     <td align="center" class="align-middle"><?php echo $data['id_kasir']; ?><br></td>
                                     <td align="center" class="align-middle">
                                     <div class="btn-group">
+                                        <?php
+                                        $encry->word = $data['id_kasir'];
+                                        $id_kasir = $encry->encr();
+                                        ?>
                                         <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
                                         Action
                                         </button>
                                         <div class="dropdown-menu">
-                                        <a class="dropdown-item" href="edit_user.php">Edit</a>
+                                        <a class="dropdown-item" href="?p=edit_user&e=<?php echo $id_kasir?>">Edit</a>
                                         <a class="dropdown-item" href="delete_user.php">Delete</a>
                                         </div>
                                     </div>
